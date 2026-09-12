47503ec9be0799ea3ba3d32b3502fb30aa90497d
Faith Wright <112712146+faithfulord1@users.noreply.github.com>
2026-09-12 14:40:39 +0100

    Add reset.js

---
+++ reset.js
@@ -0,0 +1,10 @@
+module.exports = {
+  run: [
+    {
+      method: "notify",
+      params: {
+        html: "This starter has no generated dependencies to remove. Your source files remain intact."
+      }
+    }
+  ]
+}
